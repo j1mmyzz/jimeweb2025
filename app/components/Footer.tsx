650ad2a67487ce0288ddcdb0404740a05cf22313
Jimmy Zheng <jime82075@gmail.com>
2025-01-29 13:36:16 -0500

    FaTwitter Deployment Issue Fixed

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,4 +1,4 @@
-import { FaGithub, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
+import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
   return (
