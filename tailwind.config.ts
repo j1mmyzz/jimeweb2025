af69468ad2c403f380c66855504144e22c2c746c
Jimmy Zheng <jime82075@gmail.com>
2025-02-01 22:42:14 -0500

    CHECKPOINT

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,17 +10,6 @@ const config: Config = {
 
   theme: {
     extend: {
-      //Infinite Scrolling Animation for Footer
-      animation: {
-        "infinite-scroll": "infinite-scroll 25s linear infinite",
-      },
-      keyframes: {
-        "infinite-scroll": {
-          from: { transform: "translateX(0)" },
-          to: { transform: "translateX(-100%)" },
-        },
-      },
-
       colors: {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
